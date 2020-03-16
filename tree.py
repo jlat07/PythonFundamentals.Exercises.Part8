9b33a004faf27acf63756f5500aca38a0777b1a6
JLAT <jlat0128@gmail.com>
2020-03-16 15:15:07 -0400

    updated code to work correctly. So that I can impliment the walk process into Part9

--- tree.py
+++ tree.py
@@ -5,7 +5,6 @@ def walk(dirname, file_handler):
     items = os.listdir(dirname)
     for item in items:
         path = os.path.join(dirname, item)
-        
         if os.path.isfile(path):
             file_handler.write(path + '\n')
         else:
